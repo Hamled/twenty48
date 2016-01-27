b1be909c491c4a6f9910eecdc8416dcbd257bc7c
Charles Ellis <hamled@hamled.name>
2016-01-27 15:23:51 -0800

    Major refactor

--- js/2048.js
+++ js/2048.js
@@ -37,7 +37,7 @@ var TWENTY48 = TWENTY48 || {
 
         for (var row = 0; row < TWENTY48.CONSTS.BOARD_SIZE; row++) {
           for (var col = 0; col < TWENTY48.CONSTS.BOARD_SIZE; col++) {
-            this.placeTile(row, col, TWENTY48.CONSTS.EMPTY_TILE);
+            this.setTile(row, col, TWENTY48.CONSTS.EMPTY_TILE);
           }
         }
 
@@ -45,120 +45,71 @@ var TWENTY48 = TWENTY48 || {
         for (var n = 0; n < TWENTY48.CONSTS.STARTING_TILES; n++) {
           var randRow = Math.floor(Math.random() * TWENTY48.CONSTS.BOARD_SIZE);
           var randCol = Math.floor(Math.random() * TWENTY48.CONSTS.BOARD_SIZE);
-          this.placeTile(randRow, randCol, this.newTile(TWENTY48.CONSTS.EMPTY_TILE));
+          var oldTile = this.getTile(randRow, randCol);
+          this.setTile(randRow, randCol, new TWENTY48.Tile(oldTile));
         }
       },
 
-      compressTileVector(tileVector, shouldReverse) {
-        // Logic to generate a new tile vector that has been compressed
-        // The tile vector might be from a row or a column on the board
-        // We assume compression always happens in the direction of
-        // decreasing indexes
-        // The compressed tile vector is then padded with empty tiles up
-        // to the dimension of the board
+      // Compress a tile row or column by removing all empty tiles,
+      // combining (adjacent, equal) tiles, and padding with empty
+      // tiles to retain proper board size
+      compressTiles(tileIter) {
+        var curTile = tileIter.next().value;
+        var nextIter = new TWENTY48.TileIterator(tileIter);
+        var nextTile = nextIter.next().value;
 
-        // If necessary, reverse the vector before and after compressing
-        if (shouldReverse) {
-          tileVector.reverse();
-        }
+        while (nextTile) {
+          switch(curTile.empty << 1 | nextTile.empty) {
 
-        // Loop through the tile vector and remove all empty tiles
-        for (var n = tileVector.length - 1; n >= 0; n--) {
-          if (tileVector[n].empty) {
-            tileVector.splice(n, 1);
+            // Next tile is empty, keep going
+            case 1:
+            case 3:
+              break;
+
+            // Only current tile is empty
+            case 2:
+              // Swap tiles
+              tileIter.setTile(nextTile);
+              nextIter.setTile(curTile);
+              curTile = nextTile;
+              break;
+
+            // Neither tile is empty
+            case 0:
+              // If the tiles are the same, combine
+              if (curTile.content == nextTile.content) {
+                tileIter.setTile(new TWENTY48.Tile(curTile));
+                nextIter.setTile(TWENTY48.CONSTS.EMPTY_TILE);
+              }
+
+              // We're done with the current tile
+              curTile = tileIter.next().value;
+              break;
           }
+
+          nextTile = nextIter.next().value;
         }
-
-        // Loop through the tile vector and combine all duplicates
-        for (n = 0; n < tileVector.length - 1; n++) {
-          var tile1 = tileVector[n];
-          var tile2 = tileVector[n + 1];
-
-          if (tile1.content == tile2.content) {
-            // TODO: Push animation event into queue for combination
-            tileVector.splice(n, 1);
-            tileVector[n] = this.newTile(tile1);
-          }
-        }
-
-        // Pad the tile vector with empty tiles
-        for (n = tileVector.length; n < TWENTY48.CONSTS.BOARD_SIZE; n++) {
-          tileVector[n] = TWENTY48.CONSTS.EMPTY_TILE;
-        }
-
-        if (shouldReverse) {
-          tileVector.reverse();
-        }
-
-        return tileVector;
-      },
-
-      buildTileVector(index, isRow) {
-        // Build a tile vector from the specified board row or column
-        var tileVector = [];
-        for (var n = 0; n < TWENTY48.CONSTS.BOARD_SIZE; n++) {
-          var row = isRow ? index : n;
-          var col = isRow ? n : index;
-          tileVector[n] = this.getTile(row, col);
-        }
-
-        return tileVector;
-      },
-
-      placeTileVector(index, isRow, tileVector) {
-        // Replace the specified board row or column with the given tile
-        // vector
-        for (var n = 0; n < TWENTY48.CONSTS.BOARD_SIZE; n++) {
-          var row = isRow ? index : n;
-          var col = isRow ? n : index;
-
-          this.placeTile(row, col, tileVector[n]);
-        }
-      },
-
-      updateRow(direction, row) {
-        // Update the specified row by first building a tile vector from
-        // it, then compressing that tile vector, and finally placing
-        // that tile vector back in this row
-        var tileVector = this.buildTileVector(row, true);
-
-        var shouldReverse = (direction == TWENTY48.CONSTS.DIR.RIGHT);
-        tileVector = this.compressTileVector(tileVector, shouldReverse);
-
-        this.placeTileVector(row, true, tileVector);
-      },
-
-      updateCol(direction, col) {
-        // Update the specified column by first building a tile vector
-        // from it, then compressing that tile vector, and finally
-        // placing that tile vector back in this column
-        var tileVector = this.buildTileVector(col, false);
-
-        var shouldReverse = (direction == TWENTY48.CONSTS.DIR.DOWN);
-        tileVector = this.compressTileVector(tileVector, shouldReverse);
-
-        this.placeTileVector(col, false, tileVector);
       },
 
       update(direction) {
         // Determine from our input direction whether we are shifting
         // horizontally or vertically
-        var n;
-        if (direction == TWENTY48.CONSTS.DIR.LEFT ||
-            direction == TWENTY48.CONSTS.DIR.RIGHT  ) {
-          // Update all rows if we are shifting horizontally
-          for (n = 0; n < TWENTY48.CONSTS.BOARD_SIZE; n++) {
-            this.updateRow(direction, n);
-          }
-        } else {
-          // Update all columns if we are shifting vertically
-          for (n = 0; n < TWENTY48.CONSTS.BOARD_SIZE; n++) {
-            this.updateCol(direction, n);
-          }
+        var vert = (direction == TWENTY48.CONSTS.DIR.UP ||
+                    direction == TWENTY48.CONSTS.DIR.DOWN);
+
+        // Compress each row or column in the given direction
+        for (var n = 0; n < TWENTY48.CONSTS.BOARD_SIZE; n++) {
+          this.compressTiles(new TWENTY48.TileIterator(this, vert ? {
+            col: n,
+            reverse: (direction == TWENTY48.CONSTS.DIR.DOWN)
+          } : {
+            row: n,
+            reverse: (direction == TWENTY48.CONSTS.DIR.RIGHT)
+          }));
         }
 
         // Count all of the empty tiles
-        const totalTiles = TWENTY48.CONSTS.BOARD_SIZE * TWENTY48.CONSTS.BOARD_SIZE;
+        var totalTiles = TWENTY48.CONSTS.BOARD_SIZE * TWENTY48.CONSTS.BOARD_SIZE;
         var emptyTiles = 0;
         for (n = 0; n < totalTiles; n++) {
           if (this.tiles[n].empty) {
@@ -170,32 +121,22 @@ var TWENTY48 = TWENTY48 || {
         var newTileLoc = Math.floor(Math.random() * emptyTiles);
         for (n = 0; n < totalTiles; n++) {
           if (this.tiles[n].empty && --newTileLoc < 0) {
-            this.tiles[n] = this.newTile(TWENTY48.CONSTS.EMPTY_TILE);
+            this.tiles[n] = new TWENTY48.Tile(this.tiles[n]);
             break;
           }
         }
       },
 
-      getTile(row, column) {
-        return this.tiles[row * TWENTY48.CONSTS.BOARD_SIZE + column];
-      },
-
-      placeTile(row, column, tile) {
-        this.tiles[row * TWENTY48.CONSTS.BOARD_SIZE + column] = tile;
-      },
-
-      newTile(currentTile) {
-        var tileContent;
-
-        if (currentTile === undefined || currentTile.empty) {
-          // We're generating a completely new tile
-          // TODO: Randomly select which tile content to use
-          tileContent = 2;
-        } else {
-          tileContent = TWENTY48.CONSTS.GENERATION_RULES[currentTile.content];
+      getTile(row, col) {
+        if (row === undefined || col === undefined) {
+          return;
         }
 
-        return new TWENTY48.Tile(tileContent);
+        return this.tiles[row * TWENTY48.CONSTS.BOARD_SIZE + col];
+      },
+
+      setTile(row, col, tile) {
+        this.tiles[row * TWENTY48.CONSTS.BOARD_SIZE + col] = tile;
       },
 
       print() {
@@ -212,21 +153,73 @@ var TWENTY48 = TWENTY48 || {
     };
   },
 
-  Tile: function(content) {
-    if(content === undefined) {
+  Tile: function(tile) {
+    var content, empty = false;
+
+    if (tile === undefined) {
       // undefined means the Empty Tile
-      return {
-        empty: true,
-        content: undefined
-      };
+      empty = true;
+    } else if(tile.empty) {
+      // We're generating a completely new tile
+      // TODO: Randomly select which tile content to use
+      content = 2;
+    } else {
+      content = TWENTY48.CONSTS.GENERATION_RULES[tile.content];
     }
 
-    // Otherwise... generate a new tile
-    // This tile should map to something in the view layer to allow for
-    // animating the tile's movement
     return {
-      empty: false,
+      empty: empty,
       content: content
+    };
+  },
+
+  // We must call next() on the iterator to receive the first value
+  TileIterator: function(board, options) {
+    var BOARD_SIZE = TWENTY48.CONSTS.BOARD_SIZE;
+    var iterStart;
+
+    // Copy constructor
+    if (board.options !== undefined) {
+      iterStart = board._iter;
+      options = board.options;
+      board = board.board;
+    }
+
+    if (options.row === undefined &&
+        options.col === undefined) {
+      throw new RangeError("Either a row index or a column index is " +
+                           "required to create a tile iterator.");
+    }
+
+    return {
+      board: board,
+      options: options,
+
+      next: function() {
+        this._iter = this._nextIter();
+        return {
+          value: this.board.getTile(this._row(), this._col()),
+          done: this._nextIter() === undefined
+        };
+      },
+      setTile: function(tile) {
+        this.board.setTile(this._row(), this._col(), tile);
+      },
+
+      _row: function() {
+        return this.options.row === undefined ? this._iter : this.options.row;
+      },
+      _col: function() {
+        return this.options.col === undefined ? this._iter : this.options.col;
+      },
+
+      _iter: iterStart === undefined ? (options.reverse ? BOARD_SIZE : -1) : iterStart,
+      _nextIter: function() {
+        if (this.options.reverse) {
+          return this._iter > 0 ? this._iter - 1 : undefined;
+        }
+        return this._iter < BOARD_SIZE - 1 ? this._iter + 1 : undefined;
+      }
     };
   }
 };
