0b66474481da38694c6458e8d9341572ee36bccf
Charles Ellis <hamled@hamled.name>
2016-02-03 10:05:17 -0800

    Split config data into its own file

--- js/twenty48/model.js
+++ js/twenty48/model.js
@@ -1,29 +1,4 @@
 var TWENTY48 = $.extend(TWENTY48, {
-
-  CONSTS: {
-    BOARD_SIZE: 4,
-    STARTING_TILES: 2,
-    GENERATION_RULES: {
-      undefined: 2,
-      2: 4,
-      4: 8,
-      8: 16,
-      16: 32,
-      32: 64,
-      64: 128,
-      128: 256,
-      256: 512,
-      512: 1024,
-      1024: 2048
-    },
-    DIR: {
-      LEFT: 0,
-      RIGHT: 1,
-      UP: 2,
-      DOWN: 3
-    }
-  },
-
   Board: function() {
     return {
       tiles: [], // ROW MAJOR
